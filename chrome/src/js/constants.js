98802af82a5838063ef6fab4d15c0613a36c7bb1
Oliver Klingefjord <oliver@kry.se>
2020-10-06 08:59:06 +0200

    It works, but is slow af

--- chrome/src/js/constants.js
+++ chrome/src/js/constants.js
@@ -1,5 +1,4 @@
 export const LABELS = "labels"
 export const MAX_SEQUENCE_COUNT = 50
 export const MIN_TEXT_LENGTH = 10
-export const MAX_TEXT_LENGTH = 1000
-export const MAX_DIRECT_CHILDREN = 10
+export const MAX_TEXT_LENGTH = 1024
--- chrome/src/js/content/extract.js
+++ chrome/src/js/content/extract.js
@@ -9,10 +9,9 @@ import {
 } from "../constants.js";
 
 (function () {
-  const nodes = findTextParentsEqualToMax()
-  console.log(nodes)
+  const nodes = textNodesSplitOnGranularity(MAX_SEQUENCE_COUNT)
   const dict = mapTextToId(nodes)
-  console.log(dict)
+
   // Doing this sequentially to speed up ML process
   for (let [key, value] of Object.entries(dict)) {
     let temp = {}
@@ -24,23 +23,22 @@ import {
 })()
 
 function mapTextToId(nodes) {
-  console.log("inside map text to id")
   let dict = {};
 
-  for (let i = 0; i < Math.min(nodes.length, MAX_SEQUENCE_COUNT); i++) {
-    const cleanedText = cleanText(nodes[i].innerText)
+  for (let i = 0; i < nodes.length; i++) {
+    const cleanedText = cleanText(nodes[i].innerText ? nodes[i].innerText : nodes[i].textContent) // textNodes has no .innerText attribute, but do have a .textContent attribute
     const tag = `$attentional_filter_candidate_${i}$`
-    if (cleanedText.length <= MIN_TEXT_LENGTH || cleanedText.length >= MAX_TEXT_LENGTH) {
+
+    if (cleanedText.length < MIN_TEXT_LENGTH) {
       continue
-    } else if (!Object.keys(dict).some(k => dict[k] === cleanedText)) {
-      // Only add to dict if no dupes of the text already exists
-      dict[tag] = cleanedText
+    } else if (!Object.keys(dict).some(k => dict[k].length === 1 && dict[k][0] === cleanedText)) {
+      // Only add to dict if no dupes of the text already exists.
+      dict[tag] = chunkString(cleanedText, MAX_TEXT_LENGTH)
     }
 
     nodes[i].classList.add(tag);
   }
 
-  console.log(dict)
   return dict;
 }
 
@@ -65,51 +63,20 @@ function api(sequences, handler) {
   });
 }
 
-function findMaximumNumberOfTextNodes() {
-  // This should definitely be done by an ML model eventually. But for the initial prototype, let's roll with some brittle manual logic
-  const containerTags = [
-    "DIV",
-    "NAV",
-    "SECTION",
-    "HEADER",
-    "FOOTER",
-    "MAIN"
-  ];
+/**
+ * Algorithm for finding text nodes
+ *    
+ * Extract all text nodes in the body
+ * Recursively replace the nodes with their parents until reaching limit @param maxAllowedNodeCount
+ *  Remove nodes which have reached the html, body or document level
+ *  Replace parent p with all the siblings to a node n if parent p contains node n
+ *  Remove duplicates
+ */
+function textNodesSplitOnGranularity(maxAllowedNodeCount) {
+  const start = new Date()
 
-  const isContainer = node => containerTags.some(t => t === node.nodeName);
-  const hasInnerText = node => node.innerText && node.innerText.length >= MIN_TEXT_LENGTH
+  const ignoreableTags = ["SCRIPT", "NOSCRIPT", "HTML", "BODY"]
 
-  let nodes = [...document.getElementsByTagName("body")]
-  let keepIterating = true;
-
-  let bestRun = []
-  let c = 0
-  while (keepIterating) {
-    console.log("In while iteration ", c++, " nodes is ", nodes, ". Amount of nodes with text content is ", nodes.reduce((acc, curr) => {
-      if (curr.innerText && curr.innerText.length > MIN_TEXT_LENGTH) ++acc
-      return acc
-    }, 0));
-
-    // get all nodes that have child nodes, get their child nodes and check that they are indeed containers and have text.
-    const newNodes = nodes
-      .filter(n => n.hasChildNodes)
-      .reduce((acc, n) => acc.concat(Array.from(n.childNodes)), [])
-      .filter(isContainer)
-      .filter(hasInnerText)
-      .concat(nodes.filter(n => !n.hasChildNodes))
-
-    if (newNodes.length < MAX_SEQUENCE_COUNT && newNodes.length != 0) {
-      if (newNodes.length > bestRun.length) bestRun = newNodes
-      nodes = newNodes
-    } else {
-      keepIterating = false
-    }
-  }
-
-  return bestRun
-}
-
-function findTextParentsEqualToMax() {
   const getTextNodes = element => {
     let node,
       list = [],
@@ -120,74 +87,71 @@ function findTextParentsEqualToMax() {
         false
       );
     while ((node = walk.nextNode())) {
-      if (node.parentElement && node.parentElement.children.length === 0) list.push(node)
+      if (node.parentElement && node.parentElement.children.length === 0 && !ignoreableTags.some(tag => tag === node.parentElement.nodeName)) list.push(node)
     }
     return list;
   };
 
   let nodes = Array.from(getTextNodes(document.getElementsByTagName("body")[0]));
 
+
   let c = 0
-  while (nodes.length >= MAX_SEQUENCE_COUNT) {
-    c = c++
+  // Recursively replace the nodes with their parents until reaching limit @param maxAllowedNodeCount
+  while (nodes.length >= maxAllowedNodeCount) {
+    c += 1
+
     nodes = nodes
       .filter(n => n.parentNode)
       .map(n => n.parentNode)
-      .filter(n => !["SCRIPT", "NOSCRIPT"].some(ntn => ntn === n.nodeName))
-    //console.log(c, ", Nodes after initial filtering: ", nodes)
-    nodes = nodes.filter(n1 => !nodes.some(n2 => n1.contains(n2) && n2 !== n1))
-    //console.log(c, ", Nodes after second filtering: ", nodes)
+      .filter(n => !ignoreableTags.some(tag => tag === n.nodeName))
+
+    for (let i = nodes.length - 1; i >= 0; i--) {
+      const p = nodes[i]
+
+      // Add up all descendendants of p recursively until one of the children is n if p is an ancestor to n
+      // Remove duplicates by turning into Set
+      const descendants = [...new Set(nodes.filter(n => p.contains(n) && p !== n).map(decendant => {
+        let children = Array.from(p.children)
+        while (!children.some(child => child === decendant) && children.length !== 0) {
+          children = children.reduce((acc, child) => !child.children ? acc : acc.concat(Array.from(child.children).filter(child => child.innerText)), [])
+        }
+
+        return children
+      }).flat(Infinity))]
+
+      if (descendants.length > 0) {
+        nodes.splice(i, 1)
+        nodes = nodes.concat(descendants)
+      }
+    }
+
+    // Remove duplicates
     nodes = [...new Set(nodes)]
+    console.log("Iteration ", c, " node length is ", nodes.length)
   }
 
+  console.log(`Finished parsing. Took ${(new Date() - start) / 1000} s`)
   return nodes
 }
 
-function cleanText(text) {
-  return text.replace(/\s\s+/g, " ").replace("↵", "").toLowerCase();
+function cleanText(str) {
+  return str.replace(/\s\s+/g, " ").replace("↵", " ").toLowerCase();
 }
 
-/**
- * DEPRECATED
- */
-function getSequences() {
-  // This should definitely be done by an ML model eventually. But for the initial prototype, let's roll with some brittle manual logic
-  const containerTags = ["DIV", "BUTTON", "UL", "OL", "NAV", "SECTION"];
+// TODO - use this!
+function checkValidString(str) {
+  return str && /^.*[a-zA-Z]+.*$/.test(str)
+}
 
-  const filter = (node) => {
-    const isContainer = containerTags.some((t) => t === node.tagName);
-    // This maybe should be recursive.
-    const hasContainerChildren = Array.from(node.childNodes).some((cn) =>
-      containerTags.some((ct) => ct === cn.nodeName)
-    );
-    return isContainer && !hasContainerChildren ?
-      NodeFilter.FILTER_ACCEPT :
-      NodeFilter.FILTER_SKIP;
-  };
+function chunkString(str, len) {
+  const size = Math.ceil(str.length / len)
+  const r = Array(size)
+  let offset = 0
 
-  const getTextNodes = (element) => {
-    let node,
-      list = [],
-      walk = document.createTreeWalker(
-        element,
-        NodeFilter.SHOW_ELEMENT,
-        filter,
-        false
-      );
-    while ((node = walk.nextNode()))
-      if (node.textContent.length >= MIN_TEXT_LENGTH) list.push(node);
-    return list;
-  };
-
-  let textNodes = Array.from(getTextNodes(document.getRootNode()));
-  textNodes.sort((a, b) => b.innerText.length - a.innerText.length);
-  let dict = {};
-
-  for (let i = 0; i < Math.min(textNodes.length, MAX_SEQUENCE_COUNT); i++) {
-    const id = `$attentional_filter_candidate_${i}$`;
-    textNodes[i].classList.add(id);
-    dict[id] = cleanText(textNodes[i].innerText);
+  for (let i = 0; i < size; i++) {
+    r[i] = str.substr(offset, len)
+    offset += len
   }
-  console.log(dict); // todo remove
-  return dict;
+
+  return r
 }
