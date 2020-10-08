a06665cecf85de5488c4c70b90aa96c23545e3a7
Oliver Klingefjord <oliver@kry.se>
2020-10-08 11:24:35 +0200

    label list from popup

--- chrome/src/app/components/ClassifyContentView.js
+++ chrome/src/app/components/ClassifyContentView.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getLabels, setLabels } from '../chromeStorage'
 
 const ClassifyContentView = props => {
+  const [labels, setLabelsState] = useState([])
+  const [inputValue, setInputValue] = useState("")
+
+  useEffect(() => getLabels().then(labelsFromStorage => setLabelsState(labelsFromStorage)), []);
+
+  const removeLabel = label => {
+    const copy = [...labels]
+    copy.splice(copy.indexOf(label), 1)
+    setLabelsState(copy)
+    setLabels(copy)
+  }
+
+  const handleFormSubmit = e => {
+    e.preventDefault()
+        const copy = [...labels]
+        copy.push(inputValue)
+        setInputValue("")
+        setLabelsState(copy)
+        setLabels(copy)
+  }
+
   return (
-    <h1>classifyContent</h1>
+    <div>
+      <h1>Classify Content</h1>
+      <ul>{labels.map(label => <li key={label}>{label} <button onClick={e => removeLabel(label)}>remove</button></li>)}</ul>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={inputValue} onChange={e => setInputValue(e.target.value)} />
+        <input type="submit" value="Add Label" />
+      </form>
+      <button>Extract text content</button>
+      <button>Show hidden</button>
+    </div>
   )
 }
 
